aadb18e144affd9a6509483e76d4874f1aaa530b
Astro <rodrigo.mulsa@outlook.com>
2021-02-21 13:47:29 +0100

    Added extra linear layer, upload test predictions to wandb, change normalize_img flag name to reflect that if the flag is called the value will be set as false

--- model/meme_uniter.py
+++ model/meme_uniter.py
@@ -12,10 +12,16 @@ class MemeUniter(nn.Module):
         super().__init__()
         self.uniter_model = uniter_model
         self.n_classes = n_classes
-        self.linear = nn.Linear(hidden_size, n_classes)
+        #Added an extra linear layer for classification
+        half_hidden_size = int(hidden_size/2)
+        self.linear_1 = nn.Linear(hidden_size,half_hidden_size)
+        self.activation = nn.ReLU()
+        self.linear_2 = nn.Linear(half_hidden_size, n_classes)
 
     def forward(self, **kwargs):
         out = self.uniter_model(**kwargs)
         out = self.uniter_model.pooler(out)
-        out = self.linear(out)
+        out = self.linear_1(out)
+        out = self.activation(out)
+        out = self.linear_2(out)
         return out
