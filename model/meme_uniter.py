2a666f93ab886a7644f16eceb645952a8273678f
Astro <rodrigo.mulsa@outlook.com>
2021-02-23 23:14:21 +0100

    Added linear layers flag

--- model/meme_uniter.py
+++ model/meme_uniter.py
@@ -8,16 +8,23 @@ class MemeUniter(nn.Module):
     def __init__(self,
                  uniter_model: UniterModel,
                  hidden_size: int,
-                 n_classes: int):
+                 n_classes: int,
+                 linear_layers : int):
         super().__init__()
         self.uniter_model = uniter_model
         self.n_classes = n_classes
+        self.linear_layers = linear_layers
         #Added an extra linear layer for classification
         half_hidden_size = int(hidden_size/2)
         quarter_hidden_size = int(half_hidden_size/2)
-        self.linear_1 = nn.Linear(hidden_size,half_hidden_size)
-        self.activation_1 = nn.ReLU()
-        self.linear_2 = nn.Linear(half_hidden_size, n_classes)
+        if self.linear_layers == 1:
+            self.linear_1 = nn.Linear(hidden_size,n_classes)
+        elif self.linear_layers ==2:
+            self.linear_1 = nn.Linear(hidden_size,half_hidden_size)
+            self.activation_1 = nn.ReLU()
+            self.linear_2 = nn.Linear(half_hidden_size, n_classes)
+        else:
+            raise  Exception('Linear layers amount invalid, please set to 1 or 2. Otherwise implement more (Wont give better score tho).')
         # self.activation_2 = nn.LeakyReLU(0.1)
         # self.linear_3 = nn.Linear(quarter_hidden_size,n_classes)
 
@@ -27,9 +34,11 @@ class MemeUniter(nn.Module):
         if kwargs["gender_race_probs"] is not None:
             gender_race_probs = kwargs["gender_race_probs"]
             out = torch.cat((out, gender_race_probs), 1) # concatenate the uniter output with gender and race probabilities
+            
         out = self.linear_1(out)
-        out = self.activation_1(out)
-        out = self.linear_2(out)
+        if self.linear_layers ==2:
+            out = self.activation_1(out)
+            out = self.linear_2(out)
         # out = self.activation_2(out)
         # out = self.linear_3(out)
         
--- train_uniter.py
+++ train_uniter.py
@@ -106,9 +106,12 @@ class TrainerUniter():
                                                               img_label_dim=IMG_LABEL_DIM)
             self.model = MemeUniter(uniter_model=base_model.uniter,
                                     hidden_size=base_model.uniter.config.hidden_size + self.config["gender_race_hidden_size"] ,
-                                    n_classes=self.config['n_classes'])
+                                    n_classes=self.config['n_classes'],
+                                    linear_layers=self.config['linear_layers'])
         else:
             self.load_model()
+        print("Uniter model")
+        print(self.model)
 
     def load_model(self):
         # Load pretrained model
@@ -123,7 +126,8 @@ class TrainerUniter():
 
         self.model = MemeUniter(uniter_model=uniter_model,
                                 hidden_size=uniter_model.config.hidden_size+self.config["gender_race_hidden_size"],
-                                n_classes=self.config['n_classes'])
+                                n_classes=self.config['n_classes'],
+                                linear_layers=self.config['linear_layers'])
         self.model.load_state_dict(checkpoint['model_state_dict'])
 
     def average_gradients(self, steps):
@@ -579,7 +583,8 @@ if __name__ == '__main__':
                         help='Add a note that can be seen in wandb')
     parser.add_argument('--gender_race_hidden_size', type=int, default=0,
                         help='Hidden size for gender and race')
-
+    parser.add_argument('--linear_layers', type=int, default=1,
+                        help='Set the amount of final linear layers after the uniter model')
                         
     args, unparsed = parser.parse_known_args()
     config = args.__dict__
